95cfe0506a8c8db9b1ab2f21193a89fa8d7ea0d5
jking11 <47322393+jking11@users.noreply.github.com>
2020-11-10 15:48:53 +0000

    Update

---
+++ lab03/CountingLoop/CountingLoop/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountingLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int counter = 1;
+            while (counter <= 10)
+            {
+                Console.WriteLine(counter);
+                counter++;
+            }
+
+            Console.ReadLine();
+
+            counter = 0;
+            while (counter < 100)
+            {
+                Console.Write(counter);
+                Console.Write(", ");
+                counter += 2;
+            }
+
+            Console.ReadLine();
+
+            counter = 100;
+            while (counter >= 0)
+            {
+                Console.Write(counter);
+                Console.Write(", ");
+                counter -= 2;
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
