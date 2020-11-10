90b03f12af90dda98c4ffdaf0f08e9e1834e7547
jking11 <47322393+jking11@users.noreply.github.com>
2020-11-10 12:41:13 +0000

    Initial update

---
+++ lab03/Decades/Decades/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+            if (age >= 20 && age <= 29)
+            {
+                Console.WriteLine("You are in your twenties");
+            }
+            else
+            {
+                Console.WriteLine("You are NOT in your twenties");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ lab03/MathsTrick/MathsTrick/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathsTrick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter a number, any number: ");
+            int userNumber = int.Parse(Console.ReadLine());
+            int startNumber = userNumber;
+
+            Console.WriteLine("Now add 1...");
+            userNumber += 1;
+            Console.WriteLine("You now have {0}... (press return)", userNumber);
+            Console.ReadKey();
+            
+            Console.WriteLine("Double it...");
+            userNumber *= 2;
+            Console.WriteLine("You now have {0}... (press return)", userNumber);
+            Console.ReadKey();
+            
+            Console.WriteLine("Now add 4...");
+            userNumber += 4;
+            Console.WriteLine("You now have {0}... (press return)", userNumber);
+            Console.ReadKey();
+
+            Console.WriteLine("Now divide by 2...");
+            userNumber /= 2;
+            Console.WriteLine("You now have {0}... (press return)", userNumber);
+            Console.ReadKey();
+            
+            Console.WriteLine("Now take away the number you first thought of...");
+            userNumber -= startNumber;
+            Console.WriteLine("You now have three : {0}", userNumber);
+            Console.ReadKey();
+
+        }
+    }
+}
